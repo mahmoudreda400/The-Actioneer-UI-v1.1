1b70f83a73ac885e353bfa73bc1831ac1328ffb1
mahmoud reda <mahmoudreda400@gmail.com>
2019-10-21 23:26:20 -0400

    upload post and it's images

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,18 +12,46 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  register(person) :Observable<any> {
-    return this.http.post(this.baseUrl+ '/register', person,  {
-      responseType: 'json'
+  register(person): Observable<any> {
+    return this.http.post(this.baseUrl + '/register', person, {
+      responseType: 'text' as 'json'
     });
   }
 
-  login(email, password) :Observable<any>{
+  login(email, password): Observable<any> {
     let body = new FormData();
     body.append('email', email);
     body.append('password', password);
-    return this.http.post(this.baseUrl + '/login', body,   {
-      responseType: 'json'
+    return this.http.post(this.baseUrl + '/login', body, {
+      responseType: 'json' as 'json'
     });
+  }
+
+  getAllCategories(): Observable<any> {
+    return this.http.get(this.baseUrl + '/getAllCategories', {
+      responseType: 'json' as 'json'
+    });
+  }
+
+  savePost(post, images): Observable<any> {
+    const token = localStorage.getItem('token');
+    console.log(token);
+    // let headers = new HttpHeaders({
+    //   'Authorization': token,
+    //   'responseType': 'json'
+    // });
+    let headers = new HttpHeaders({
+      'Authorization': token
+    });
+    const options = { responseType: 'text' as 'json', headers };
+    let body = new FormData();
+    for (let image of images) {
+      body.append('images', image);
+    }
+    body.append("post", new Blob([JSON.stringify(post)],
+      {
+        type: "application/json"
+      }));
+    return this.http.post(this.baseUrl + '/addPost', body, options);
   }
 }
